4e31f7563ed9df9f53bb7a2580c748be93d5bf12
Chenziyuan686508 <yuan80886@gmail.com>
2025-03-04 15:14:07 +0800

    try

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,12 +1,18 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from "vue-router";
 import HomeView from "../views/HomeView.vue";
 import LoginView from "../views/LoginView.vue";
+import MainView from "../views/MainView.vue";
 
 const routes: Array<RouteRecordRaw> = [
   {
     path: "/",
     name: "home",
     component: LoginView,
+  },
+  {
+    path: "/main", // 主界面路径
+    name: "main", // 路由名称
+    component: MainView, // 路由指向的主界面组件
   },
 ];
 

3160df1a21495dc12b37eec91b1a1789d6854c45
bzh <12210428@mail.sustech.edu.cn>
2025-03-27 19:33:05 +0800

    相册界面&相册详情界面&照片查看界面

---
+++ .history/src/App_20250327192343.vue
@@ -0,0 +1,26 @@
+<template>
+  <transition name="fade">
+    <router-view />
+  </transition>
+</template>
+
+<style>
+*,
+*::after,
+*::before {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  user-select: none;
+}
+
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.2s ease;
+}
+
+.fade-enter-from,
+.fade-leave-to {
+  opacity: 0;
+}
+</style>
---
+++ .history/src/router/index_20250327110131.ts
@@ -0,0 +1,68 @@
+import { createRouter, createWebHistory, RouteRecordRaw } from "vue-router";
+import HomeView from "../views/HomeView.vue";
+import LoginView from "../views/LoginView.vue";
+import UsercenterView from "@/views/home/UsercenterView.vue";
+import DiscoverView from "@/views/home/DiscoverView.vue";
+import PhotosView from "@/views/home/PhotosView.vue";
+import ImageEditor from "@/views/ImageEditor.vue";
+import AlbumsView from "@/views/home/AlbumsView.vue";
+import AlbumDetailView
+ from "@/views/home/AlbumDetailView.vue";
+const routes: Array<RouteRecordRaw> = [
+  {
+    path: "/",
+    name: "login",
+    component: LoginView,
+  },
+  {
+    path: "/home",
+    name: "home",
+    component: HomeView,
+    redirect: "/home/discover",
+    children: [
+      {
+        path: "discover",
+        name: "discover",
+        component: DiscoverView,
+      },
+      {
+        path: "photos",
+        name: "photos",
+        component: PhotosView,
+      },
+      {
+        path: "usercenter",
+        name: "usercenter",
+        component: UsercenterView,
+      },
+      {
+        path: "edit",
+        name: "edit",
+        component: ImageEditor,
+      },
+      {
+        path: "albums",
+        name: "albums",
+        component: AlbumsView,
+      },
+    ],
+  },
+];
+
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  routes,
+});
+
+router.beforeEach((to, from, next) => {
+  const isAuthenticated = localStorage.getItem("jwtToken");
+
+  if (!isAuthenticated && to.path !== "/") {
+    next("/");
+  } else if (isAuthenticated && to.path === "/") {
+    next("/home");
+  } else {
+    next();
+  }
+});
+export default router;

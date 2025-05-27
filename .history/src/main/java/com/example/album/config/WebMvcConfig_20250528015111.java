3f2240317ef64fd8ecc4825b3f4cda2b7e92520e
bzh <12210428@mail.sustech.edu.cn>
2025-05-28 01:52:22 +0800

    add patch

---
+++ .history/src/main/java/com/example/album/config/WebMvcConfig_20250528015111.java
@@ -0,0 +1,36 @@
+package com.example.album.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+
+    /**
+     * 配置跨域
+     */
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+<<<<<<< HEAD
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH")
+=======
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS","PATCH")
+>>>>>>> b0294f46ab7286f2d042908f1ef00f46049d20bc
+                .allowedHeaders("*")
+                .maxAge(3600);
+    }
+
+    /**
+     * 配置静态资源映射
+     */
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        // 可以根据实际需求配置本地文件系统路径
+        registry.addResourceHandler("/uploads/**")
+                .addResourceLocations("file:uploads/");
+    }
+}
